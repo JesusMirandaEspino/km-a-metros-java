e1adbfea60ca29c8789f60dd5a50ba727084eafb
Jose de Jesus Miranda Espino <jesus.voluntadheredada.human@gmail.com>
2021-05-24 23:02:45 -0500

    ciclo java

---
+++ ciclo.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class ciclo{
+
+    public static void main( String[] args ){
+
+        Integer numero;
+
+        for( numero = 0; numero <= 100; numero = numero + 5 ){
+            System.out.println(   numero + "\n" );
+        }
+
+    }
+
+}
